f54e5e6862fd020405f682b5ba335bfb7e0a88ac
SamSlotemaker <sam.slotemaker@ziggo.nl>
2020-02-28 09:57:30 +0100

    commit test

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+let data = {
+  title: "Datingapp",
+  page: "About",
+  name: "Sam Slotemaker"
+}
+
+app
+  .use(express.static('public'))
+  .set('view engine', 'ejs')
+  .set('views', 'view')
+
+
+
+
+app.get('/about', (req, res) => {
+  res.render('about.ejs', {
+    data
+  })
+})
+
+app.get('/finding', (req, res) => {
+  res.sendFile('public/finding.html' , { root : __dirname});
+})
+
+app.get('*', (req, res) => {
+  res.end('Error: 404 - Page not found');
+})
+
+app.listen(port, () => console.log(`app running on port: ${port}`));
