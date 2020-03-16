773e968fdc0d301403a0aa54ce94f32d9bfac59f
Sam <sam.slotemaker@ziggo.nl>
2020-03-16 18:52:12 +0100

    loop ipv aparte declaraties

--- server.js
+++ server.js
@@ -4,12 +4,6 @@ const port = 3001;
 require('dotenv').config();
 const session = require('express-session');
 
-app.use(session({
-  'secret': '343ji43j4nasaSSAs3jn4jk3n',
-  saveUninitialized: false,
-  resave: false
-}))
-
 //database configuratie
 let collection = null;
 const MongoClient = require('mongodb').MongoClient;
@@ -34,10 +28,14 @@ app
   .use(express.urlencoded({
     extended: false
   }))
+  .use(session({
+    'secret': '343ji43j4nasaSSAs3jn4jk3n',
+    saveUninitialized: false,
+    resave: false
+  }))
   .post('/sendImage', sendImage)
   .get('/finding', finding)
   .get('/list', list)
-
   .get('*', error404)
 
 
@@ -56,21 +54,21 @@ let questionNumber = 0;
 
 let data = {
   title: "Datingapp",
-  name: "Sam Slotemaker",
-  imageUrl1: images[0],
-  imageUrl2: images[1],
-  imageUrl3: images[2],
-  imageUrl4: images[3],
-  imageUrl5: images[4],
-  imageUrl6: images[5]
+  name: "Sam Slotemaker"
 }
 
+//vul data met foto's; op imageUrlX
+for(let i =0; i < images.length; i++){
+  let index = "imageUrl" + (i+1);
+
+  data[index] = images[i];
+}
 
 
 function list(req, res) {
   collection.findOne({user: "SamSloot"}, done)
 
-  function done(err, data) {
+  function done(err, data1) {
     if(err){
       next(err)
     }
